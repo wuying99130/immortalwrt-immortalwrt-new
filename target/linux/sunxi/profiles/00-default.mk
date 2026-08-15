93952e395f8696982fdc1f0b298ee8c76c51c7da
wuying99130 <46892042+wuying99130@users.noreply.github.com>
2026-08-15 20:08:51 +0800

    Add profile for FriendlyARM NanoPi R1S-H3

--- target/linux/sunxi/profiles/00-default.mk
+++ target/linux/sunxi/profiles/00-default.mk
@@ -21,3 +21,14 @@ define Profile/Default/Description
   Default profile with package set compatible with most boards.
 endef
 $(eval $(call Profile,Default))
+
+define Profile/nanopi-r1s-h3
+  NAME:=FriendlyARM NanoPi R1S-H3
+  PACKAGES:= \
+        kmod-usb-net-rtl8152 \
+        kmod-rtl8189es \
+        kmod-usb2 \
+        kmod-usb-ohci \
+        kmod-usb-ehci
+endef
+$(eval $(call Profile,nanopi-r1s-h3))
